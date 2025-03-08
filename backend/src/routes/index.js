9589702bf0dd7d38f7da977bde8f29e421d5ede9
tr-nhan <letrungnhan021704@gmail.com>
2025-03-08 11:27:31 +0700

    Initial commit

---
+++ backend/src/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const homeController = require("../controllers/HomeController");
+
+/* GET home page. */
+router.get('/', homeController.get);
+
+module.exports = router;
